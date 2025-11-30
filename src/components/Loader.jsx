060563c949f3438ba4c12e61620d5880d3a568f8
Mr. AKS <aksnetlink@gmail.com>
2025-11-30 18:11:28 +0530

    13

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { motion } from 'motion/react'
 import assets from '../assets/assets'
-import { PropagateLoader } from 'react-spinners'
-
 
 const Loader = ({ theme }) => {
   return (
@@ -12,26 +10,85 @@ const Loader = ({ theme }) => {
       transition={{ duration: 0.5 }}
       className='fixed inset-0 z-50 flex items-center justify-center bg-white dark:bg-gray-900'
     >
-      <div className='flex flex-col items-center gap-8'>
-        {/* Logo and Text Container */}
-        <div className='flex items-center gap-1 overflow-hidden'>
-          {/* Loader Logo */}
-          <motion.img
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            src={assets.loader_logo}
-            alt="Logo"
-            className='h-10 sm:h-12 w-auto'
-          />
+      <style>
+        {`
+          .loader-container {
+            position: relative;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          
+          .loader {
+            position: absolute;
+            transform: rotateZ(45deg);
+            perspective: 1000px;
+            border-radius: 50%;
+            width: 120px;
+            height: 120px;
+            color: #5044E5;
+          }
+          .loader:before,
+          .loader:after {
+            content: '';
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: inherit;
+            height: inherit;
+            border-radius: 50%;
+            transform: rotateX(70deg);
+            animation: 1s spin linear infinite;
+          }
+          .loader:after {
+            color: #4d8cea;
+            transform: rotateY(70deg);
+            animation-delay: .4s;
+          }
 
+          @keyframes spin {
+            0%,
+            100% {
+              box-shadow: .4em 0px 0 0px currentcolor;
+            }
+            12% {
+              box-shadow: .4em .4em 0 0 currentcolor;
+            }
+            25% {
+              box-shadow: 0 .4em 0 0px currentcolor;
+            }
+            37% {
+              box-shadow: -.4em .4em 0 0 currentcolor;
+            }
+            50% {
+              box-shadow: -.4em 0 0 0 currentcolor;
+            }
+            62% {
+              box-shadow: -.4em -.4em 0 0 currentcolor;
+            }
+            75% {
+              box-shadow: 0px -.4em 0 0 currentcolor;
+            }
+            87% {
+              box-shadow: .4em -.4em 0 0 currentcolor;
+            }
+          }
+        `}
+      </style>
+      <div className='loader-container'>
+        {/* 3D Spinner Behind/Around Logo */}
+        <div className="loader"></div>
 
-        </div>
-
-        {/* Propagate Loader */}
-        <div className="mt-4">
-          <PropagateLoader color="var(--color-primary)" size={15} />
-        </div>
+        {/* Logo in Center */}
+        <motion.img
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          src={assets.loader_logo}
+          alt="Logo"
+          className='h-12 sm:h-14 w-auto relative z-10'
+        />
       </div>
     </motion.div>
   )
