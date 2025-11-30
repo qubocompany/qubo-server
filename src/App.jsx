6e67d2893d1b9049c18fd2e523395e6b21ff94cf
Mr. AKS <aksnetlink@gmail.com>
2025-11-30 11:19:30 +0530

    4

--- src/App.jsx
+++ src/App.jsx
@@ -86,11 +86,11 @@ const App = () => {
       <Footer theme={theme} />
 
       {/* Custom Cursor Ring */}
-      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'
+      <div ref={outlineRef} className='hidden lg:block fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'
         style={{ transition: 'transform 0.1s ease-out' }}></div>
 
       {/* Custom Cursor Dot */}
-      <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]'></div>
+      <div ref={dotRef} className='hidden lg:block fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]'></div>
 
       {/* WhatsApp Floating Button */}
       <a
